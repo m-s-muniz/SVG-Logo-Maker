6b2766d647f83a6f0fc3acba6d1bc6f92219e50b
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-04-05 14:17:49 -0500

    added new varibles to store answers with some error logic and conformation.  Added a SVG class with a construtor. Added a circle shapes test for shapes.test.js and a circle shape class for shapes.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
 
 //An array of questions for user input.
@@ -26,13 +27,60 @@ const questions = [
     choices: ["Circle", "Triangle", "Square"],
 },
 ];
+// Svg class that has a constructor with methods for rendering and setting the text and shape elements in the SVG string.
+class Svg {
+  constructor() {
+      this.textElement = '';
+      this.shapeElement = '';
+  }
+  render() {
+      return `TEST`;
+  }
+}
 
-
-
+//   Function to initialize and prompt the user questions
 function init() {
   inquirer.prompt(questions)
+  .then(answers => {
+    //checking for no more than 3 charaters
+    var characters = "";
+    if (answers.text.length > 0 && answers.text.length < 4) {
+        characters = answers.text;
+    } else {
+        console.log("Invalid number of Charaters, Please enter 1-3 Characters");
+        return;
+    }
+    console.log("Selected text letters: [" + characters + "]");
+    //  font color
+    fontColor = answers.textColor;
+    console.log("Selected font color: [" + fontColor + "]");
+    //  shape color
+    shapeColr = answers.shapeColor;
+    console.log("Selected shape color: [" + shapeColr + "]");
+    //  shape type
+    shapeType = answers.shape;
+    console.log("Selected shape:  [" + shapeType + "]");
+    // shape
+    let shape;
+    if (shapeType == "Square") {
+        shape = new Square();
+        console.log("You chose a Square Logo");
+    } else if (shapeType == "Circle") {
+        shape = new Circle();
+        console.log("You chose a Circle Logo");
+    } else if (shapeType == "Triangle") {
+        shape = new Triangle();
+        console.log("You chose a Triangle Logo");
+    }
+    // Create a new Svg instance and add the shape and text elements to it
+      var svg = new Svg();
+      svgString = svg.render();
+      //Console.log SVG string
+      console.log("Here is the SVG String:  " + svgString);
+ 
 
-
+})
+.catch((err) => console.log(err));
 }
 
 init();
